78aa238608e39357313062e9ae4ef32874d89197
Vladimir N. Solovyov <vladimir.n.solovyov@gmail.com>
2018-07-31 07:51:20 +0300

    - systemd.service units were added - console endpoint name change

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
         name = "aiokafka-websocket",
         description = "Websocket Server/reader for the Kafka Protocol (aiokafka -> aiohttp; KFK>0.10)",
-        version = "0.1.1",
+        version = "0.1.2",
         author = "Vladimir N. Solovyov",
         author_email = "vladimir.n.solovyov@gmail.com",
 
         packages = ["aiokafka_websocket_reader"],
         entry_points = {"console_scripts": [
-            "aiokafka_wss = aiokafka_websocket_reader.__main__:main",
+            "aiokafka-wss = aiokafka_websocket_reader.__main__:main",
         ]},
 
         install_requires = ["aiokafka", "aiohttp"],
